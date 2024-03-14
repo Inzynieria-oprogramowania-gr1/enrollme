f31c097e09a57f0aae094640f8c3a259a238c3b0
kpiotr6 <piotrek@komlan.pl>
2024-03-14 18:48:47 +0100

    Add StudentService, add POST method for list of emails

--- backend/src/main/java/com/company/project/repository/StudentRepository.java
+++ backend/src/main/java/com/company/project/repository/StudentRepository.java
@@ -1,9 +1,13 @@
 package com.company.project.repository;
 
 import com.company.project.entity.Student;
+
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends CrudRepository<Student, Integer> {
+  Optional<Student> findStudentByEmail(String email);
 }
