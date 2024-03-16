d706c4c0ec0d666a1c038a9be7a493ace501d840
kpiotr6 <piotrek@komlan.pl>
2024-03-16 23:18:55 +0100

    Standarized error http responses

--- backend/src/main/java/com/company/project/exception/GlobalDefaultExceptionHandler.java
+++ backend/src/main/java/com/company/project/exception/GlobalDefaultExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.company.project.exception;
 
 import java.io.FileNotFoundException;
-
+import java.net.URISyntaxException;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,8 +16,8 @@ import lombok.val;
 @ControllerAdvice
 class GlobalDefaultExceptionHandler {
   public static final String DEFAULT_ERROR_VIEW = "error";
-
-  @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR, reason="Someone stupid forgot to implement this exception :(")
+  
+  @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR, reason="Server error :(")
   @ExceptionHandler(value = Exception.class)
   public void defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
     if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null){
