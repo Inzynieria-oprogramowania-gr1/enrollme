9b4d4d55ed2a73e02ff36e75d13075e7aa4dd7d3
Adrian Stahl <adrianstahl.2@icloud.com>
2024-03-16 17:05:57 +0100

    add routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import BrowserRouter from "react-router-dom";
+import {Route, Routes, useNavigate} from "react-router-dom";
 import TimeTable from "./teacher/TimeTable";
 import Results from "./teacher/Results";
 import "./App.css";
@@ -8,13 +8,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 
 function App() {
+
+  const navigate = useNavigate();
+
   const handleResultsButtonClick = () => {
-    setShowTimeTable(false);
-    setShowResults(true);
+    navigate("/results")
   }
   const handleMakeTimeTableClick = () => {
-    setShowResults(false);
-    setShowTimeTable(true);
+    navigate("/timetable")
   }
 
 
@@ -38,7 +39,10 @@ function App() {
           <button className="btn btn-primary" onClick={handleMakeTimeTableClick}>MAKE TIMETABLE</button>
           <button className="btn btn-primary" onClick={handleResultsButtonClick}>RESULTS</button>
       </header>
-        {showResults ? <Results /> : showTimeTable ? <TimeTable /> : null}
+      <Routes>
+        <Route path="/results" element={<Results />} />
+        <Route path="/timetable" element={<TimeTable />} />
+      </Routes>
     </div>
   );
 }
