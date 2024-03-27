587e1f0535e8525d3d48977bad62e94b93f88e89
Ivan Zarzhitski <ivan.zarzhitski@gmail.com>
2024-03-27 23:32:54 +0100

    Add dependencies for spring mail service. Add mail service and endpoint(/demo/send) for students email notification

---
+++ backend/src/main/java/com/company/project/mailService/EmailServiceImpl.java
@@ -0,0 +1,29 @@
+package com.company.project.mailService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailServiceImpl{
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public EmailServiceImpl(JavaMailSender mailSender){
+      this.emailSender = mailSender;
+    }
+
+    @Async
+    public void sendEmail(String to, String subject, String body) {
+      SimpleMailMessage message = new SimpleMailMessage();
+      message.setTo(to);
+      message.setSubject(subject);
+      message.setText(body);
+
+      emailSender.send(message);
+  }
+}
