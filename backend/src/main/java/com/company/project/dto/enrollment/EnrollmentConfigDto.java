29454647b9106649bbe33f49c25dc8d1d6ec9f26
Adam Mężydło <adamkmezydlo@gmail.com>
2024-03-31 14:45:25 +0200

    Fix dates

--- backend/src/main/java/com/company/project/dto/enrollment/EnrollmentConfigDto.java
+++ backend/src/main/java/com/company/project/dto/enrollment/EnrollmentConfigDto.java
@@ -1,6 +1,12 @@
 package com.company.project.dto.enrollment;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
-public record EnrollmentConfigDto(int groupAmount, LocalDateTime deadline) {
+public record EnrollmentConfigDto
+        (int groupAmount,
+         @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+         LocalDateTime deadline
+        ) {
 }
