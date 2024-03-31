e2c7733b1482ec5ec631538fa3fa6079bca8e7ec
Adam Mężydło <adamkmezydlo@gmail.com>
2024-03-31 15:17:52 +0200

    remove unused import

--- backend/src/main/java/com/company/project/entity/Enrollment.java
+++ backend/src/main/java/com/company/project/entity/Enrollment.java
@@ -5,7 +5,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
 
 import java.time.LocalDateTime;
 import java.util.List;
