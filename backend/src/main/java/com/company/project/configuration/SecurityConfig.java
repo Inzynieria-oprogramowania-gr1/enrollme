50c92e48f82e575c268090472ba5bde64929d886
Adam Mężydło <adamkmezydlo@gmail.com>
2024-04-01 01:59:02 +0200

    update teacher credentials

--- backend/src/main/java/com/company/project/configuration/SecurityConfig.java
+++ backend/src/main/java/com/company/project/configuration/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req
-                        .requestMatchers("/hello")
+                        .requestMatchers("/students/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -35,4 +35,4 @@ FROM (SELECT 0 AS weekday UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT
       SELECT '18:30:00', '20:00:00') t;
 
 INSERT INTO mydb._user(email, password, role)
-VALUES ('miidzik@agh.edu.pl', '$2a$12$bhFdyY.oYjRuw0Kga1LZcOwMOPORv4APAjYsiy3zE17GYL/CjoS1q', 'TEACHER')
+VALUES ('miidzik@agh.edu.pl', '$2a$12$uTyWiJKnOxy3hYmAeDhTMuqO1xi4B.qKE.2vp.27RQ7XupcNRsjFS', 'TEACHER')
