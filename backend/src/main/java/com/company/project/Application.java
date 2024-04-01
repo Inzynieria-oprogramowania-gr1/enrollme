bba0d2dfa8c5bfadae516dd16066e9cda2d37d76
Adam Mężydło <adamkmezydlo@gmail.com>
2024-04-01 02:00:27 +0200

    delete temporary endpoint

--- backend/src/main/java/com/company/project/Application.java
+++ backend/src/main/java/com/company/project/Application.java
@@ -2,7 +2,6 @@ package com.company.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,11 +11,4 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello";
-    }
-
 }
