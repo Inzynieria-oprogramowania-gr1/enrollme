4ae9ccd6234a01d09e49fb246c3ecfcb1997d653
Adam Mężydło <adamkmezydlo@gmail.com>
2024-04-08 22:30:55 +0200

    add reset endpoint

--- backend/src/main/java/com/company/project/repository/StudentRepository.java
+++ backend/src/main/java/com/company/project/repository/StudentRepository.java
@@ -10,4 +10,7 @@ import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Optional<Student> findByEmail(String email);
+
+
+    void deleteAllByIdGreaterThan(Long id);
 }
