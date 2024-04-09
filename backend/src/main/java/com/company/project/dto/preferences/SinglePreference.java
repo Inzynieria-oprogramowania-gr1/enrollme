3fb5db67119c5442fe0857de15a6ce749396ee7f
Adam Mężydło <adamkmezydlo@gmail.com>
2024-04-10 01:16:14 +0200

    change StudentPreferencesDto json format

---
+++ backend/src/main/java/com/company/project/dto/preferences/SinglePreference.java
@@ -0,0 +1,33 @@
+package com.company.project.dto.preferences;
+
+public record SinglePreference(PreferredTimeslot timeslot, boolean selected, String note) {
+}
+
+
+// jak przesłać preferencje??
+
+// StudentPreferencesDto (Long id, String email, List<SinglePreferenceDto> preferences)
+// SinglePreferenceDto (PreferredTimeSlot timeslot, boolean selected, String note)
+// PreferredTimeSlot (Weekday weekday, LocalTime startTime, LocalTime endTime)
+
+/*
+*
+* { student_id: 1,
+*   email: "example@gmail.com",
+*   preferences: [
+*     {
+*       timeslot: {
+*         weekday: "Monday",
+*         start_time: "08:00",
+*         end_time: "09:30",
+*       },
+*       selected: true,
+*       note: "It is ok, but I prefer another one"
+*     },
+*     ...
+*     {...}
+*   ]
+*
+*
+*
+* */
