560266a7d8769c4710fc6f6dd4c48c25cf900d16
Adam Mężydło <adamkmezydlo@gmail.com>
2024-04-10 14:08:19 +0200

    rebase scrum-75 to main

--- backend/src/main/java/com/company/project/configuration/EventListeners.java
+++ backend/src/main/java/com/company/project/configuration/EventListeners.java
@@ -8,7 +8,6 @@ import com.company.project.schedulers.ScheduledTasks;
 import com.company.project.schedulers.TaskType;
 import com.company.project.service.ShareLinkService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
