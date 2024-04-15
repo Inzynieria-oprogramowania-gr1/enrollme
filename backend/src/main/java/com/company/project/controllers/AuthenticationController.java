18f27046b8c22fdd1a497d3e77cec34c3a02f099
Ivan Zarzhitski <ivan.zarzhitski@gmail.com>
2024-04-15 15:44:17 +0200

    Return cross-origin

--- backend/src/main/java/com/company/project/controllers/AuthenticationController.java
+++ backend/src/main/java/com/company/project/controllers/AuthenticationController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin
 @SecurityRequirement(name = "basicAuth")
 public class AuthenticationController {
 
